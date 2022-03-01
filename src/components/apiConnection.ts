84ed2e218f2f014f898a0ea1724b179a90b2e527
gordongecco <delldani001@gmail.com>
2022-03-01 12:17:48 +0100

    ref

--- src/components/apiConnection.ts
+++ src/components/apiConnection.ts
@@ -1,5 +1,5 @@
 export const sendData = (date: string, score: number) => {
-  fetch("http://localhost:5000/", {
+  fetch("http://matekapi.herokuapp.com", {
     method: "POST",
     headers: {
       Accept: "application/json",
